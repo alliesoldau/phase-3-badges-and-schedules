196ee30a79406456bc15bedbdfb1b26c3a08a0c8
Allie Soldau <alliesoldau@gmail.com>
2022-12-11 06:40:32 -0500

    commit tree bb891c65a372a53be00c84d402411da7dcacc48b

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end 
+end
+
+def assign_rooms array
+    counter = 0
+    array.map do |name|
+        counter +=1
+        "Hello, #{name}! You'll be assigned to room #{counter}!"
+    end
+end
+
+def printer array
+    badges = batch_badge_creator array
+    badges.each do |name|
+        puts name
+    end
+    rooms = assign_rooms array
+    rooms.each do |room|
+        puts room
+    end
+end
